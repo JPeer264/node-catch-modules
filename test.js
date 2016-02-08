4ea55cd62ed21278b5dee0d8d4a369a3c6506f4f
jpeer264 <jan.oster94@gmail.com>
2016-02-08 15:27:42 +0100

    sorted test in async and sync

--- test.js
+++ test.js
@@ -4,56 +4,110 @@ var mocha = require('mocha'),
     _ = require('lodash'),
     findModules = require('./');
 
+var testModule = 'catch-modules-test';
+var testDependency = 'mocha';
+var searchHint = 'test';
+
 // todo test catch global modules
 
-describe('find some modules by name', function () {
-    it('should find 1 module by name', function (done) {
-        expect(findModules.byNameSync('test').length).to.equal(1);
+describe('SYNC Version', function () {
 
-        findModules.byName('test', function(err, data) {
-            expect(data.length).to.equal(1);
-            done();
-        });
+    // byNameSync
+    it('+ byNameSync', function (done) {
+        expect(findModules.byNameSync(searchHint).length).to.equal(1);
+
+        done();
     });
 
-    it('should find module "test" by name', function (done) {
-        expect((findModules.byNameSync('test'))[0].name).to.equal('catch-modules-test');
-        expect((findModules.byNameSync('test'))[0].keyword).to.equal('test');
+    it('should have correct values in object', function (done) {
+        expect((findModules.byNameSync(searchHint))[0].name).to.equal(testModule);
+        expect((findModules.byNameSync(searchHint))[0].keyword).to.equal(searchHint);
 
         // todo async version
         done();
     });
+
+    it('should find a global module', function (done) {
+        expect(findModules.byNameSync(true, testDependency).length).to.equal(1);
+
+        done();
+    });
+
+    // byDependencySync
+    it('+ byDependencySync', function (done) {
+        expect(findModules.byDependencySync(testDependency).length).to.equal(1);
+
+        done();
+    });
+
+    it('should have correct values in object', function (done) {
+        var listOfModules = findModules.byDependencySync(testDependency);
+
+        _.forEach(listOfModules, function(module, index) {
+            if (module.module === testModule) {
+                expect(module.name).to.equal(testDependency);
+                expect(module.keyword).to.equal(testDependency);
+            }
+        });
+
+        done();
+    });
+
+    it('should find a global module', function (done) {
+        expect(findModules.byDependencySync(true, testDependency).length).to.equal(1);
+
+        done();
+    });
 });
 
-describe('find some modules by dependency', function () {
-    it('should find 1 module by dependency', function (done) {
-        expect(findModules.byDependencySync('mocha').length).to.equal(1);
+describe('ASYNC Version', function () {
+    // todo add fail if 'err'
 
-        findModules.byDependency('mocha', function(err, data) {
+    // byName
+    it('+ byName', function (done) {
+        findModules.byName(searchHint, function(err, data) {
             expect(data.length).to.equal(1);
             done();
         });
     });
 
-    it('should find module in dependency of catch-modules-test', function (done) {
-        var listOfModules = findModules.byDependencySync('mocha');
-
-        _.forEach(listOfModules, function(module, index) {
-            if (module.module === 'catch-modules-test') {
-                expect(module.name).to.equal('mocha');
-                expect(module.keyword).to.equal('mocha');
-            }
+    it('should have correct values in object', function (done) {
+        findModules.byName(searchHint, function (err, data) {
+            expect(data[0].name).to.equal(testModule);
+            expect(data[0].keyword).to.equal(searchHint);
+        
+            done();
         });
+    });
 
-        findModules.byDependency('mocha', function (err, data) {
+    it('should find a global module', function (done) {
+        findModules.byName(true, testDependency, function (err, data) {
+            expect(data).to.have.length.of.at.least(1);
+
+            done();
+        });
+    });
+
+    // byDependency
+    it('+ byDependency', function (done) {
+        findModules.byDependency(testDependency, function(err, data) {
+            expect(data.length).to.equal(1);
+            done();
+        });
+    });
+
+    it('should have correct values in object', function (done) {
+        var listOfModules = findModules.byDependencySync(testDependency);
+
+        findModules.byDependency(testDependency, function (err, data) {
             _.forEach(listOfModules, function(module, index) {
-                if (module.module === 'catch-modules-test') {
-                    expect(module.name).to.equal('mocha');
-                    expect(module.keyword).to.equal('mocha');
+                if (module.module === testModule) {
+                    expect(module.name).to.equal(testDependency);
+                    expect(module.keyword).to.equal(testDependency);
 
                     done();
                 }
             });
         });
-    });
-})
+    });     
+});
