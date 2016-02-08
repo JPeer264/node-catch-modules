4c23d85fb47e94275025999fea93e19a9606fb40
jpeer264 <jan.oster94@gmail.com>
2016-02-08 16:37:22 +0100

    Added tests

--- test.js
+++ test.js
@@ -10,10 +10,8 @@ var searchHint = 'test';
 
 // todo test catch global modules
 
-describe('SYNC Version', function () {
-
-    // byNameSync
-    it('+ byNameSync', function (done) {
+describe('+byNameSync', function () {
+    it('should find 1 module', function (done) {
         expect(findModules.byNameSync(searchHint).length).to.equal(1);
 
         done();
@@ -33,8 +31,47 @@ describe('SYNC Version', function () {
         done();
     });
 
-    // byDependencySync
-    it('+ byDependencySync', function (done) {
+    it('should return an empty array if nothing is found', function (done) {
+        var fakeModule = findModules.byNameSync('you-can-never-find-me');
+
+        expect(fakeModule).to.be.an('array');
+        expect(fakeModule).to.be.empty;
+
+        done();
+    });
+});
+
+describe('+ byName', function () {
+    // todo add fail if 'err'
+
+    // byName
+    it('should find 1 module', function (done) {
+        findModules.byName(searchHint, function(err, data) {
+            expect(data.length).to.equal(1);
+            done();
+        });
+    });
+
+    it('should have correct values in object', function (done) {
+        findModules.byName(searchHint, function (err, data) {
+            expect(data[0].name).to.equal(testModule);
+            expect(data[0].keyword).to.equal(searchHint);
+        
+            done();
+        });
+    });
+
+    it('should find a global module', function (done) {
+        findModules.byName(true, testDependency, function (err, data) {
+            expect(data).to.have.length.of.at.least(1);
+
+            done();
+        });
+    });
+});
+
+describe('+byDependencySync', function () {
+    it('should find 1 module', function (done) {
         expect(findModules.byDependencySync(testDependency).length).to.equal(1);
 
         done();
@@ -60,36 +97,8 @@ describe('SYNC Version', function () {
     });
 });
 
-describe('ASYNC Version', function () {
-    // todo add fail if 'err'
-
-    // byName
-    it('+ byName', function (done) {
-        findModules.byName(searchHint, function(err, data) {
-            expect(data.length).to.equal(1);
-            done();
-        });
-    });
-
-    it('should have correct values in object', function (done) {
-        findModules.byName(searchHint, function (err, data) {
-            expect(data[0].name).to.equal(testModule);
-            expect(data[0].keyword).to.equal(searchHint);
-        
-            done();
-        });
-    });
-
-    it('should find a global module', function (done) {
-        findModules.byName(true, testDependency, function (err, data) {
-            expect(data).to.have.length.of.at.least(1);
-
-            done();
-        });
-    });
-
-    // byDependency
-    it('+ byDependency', function (done) {
+describe('+ byDependency', function () {
+    it('should find 1 Module', function (done) {
         findModules.byDependency(testDependency, function(err, data) {
             expect(data.length).to.equal(1);
             done();
@@ -109,5 +118,5 @@ describe('ASYNC Version', function () {
                 }
             });
         });
-    });     
+    });
 });
